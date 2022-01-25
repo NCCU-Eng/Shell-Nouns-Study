988799ace11fd7252962e1eab56020efc454938e
TWSC-Aug <35834182+twsc-aug@users.noreply.github.com>
2022-01-25 20:57:24 +0800

    update columns

--- scripts/compile.py
+++ scripts/compile.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv("../corpus/shell_noun.csv")
+df.columns = ['cate1', 'cate2', 'cate3', 'trans_ch', 'trans_jp', 'trans_cz', "sent_eng", "sent_jp", "sent_cz", "sent_ch"]
 
 with open('../corpus/shell_noun.json', 'w', encoding='utf-8') as file:
     df.to_json(file, force_ascii=False)
