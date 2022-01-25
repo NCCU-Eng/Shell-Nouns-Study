df1292ae5f52257ad118ef834428a614930f4cd3
TWSC-Aug <35834182+twsc-aug@users.noreply.github.com>
2022-01-25 21:10:03 +0800

    update html

--- scripts/compile.py
+++ scripts/compile.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 df = pd.read_csv("../corpus/shell_noun.csv")
 df.columns = ['cate1', 'cate2', 'cate3', 'trans_ch', 'trans_jp', 'trans_cz', "sent_eng", "sent_jp", "sent_cz", "sent_ch"]
+df.fillna('', inplace=True)
 
 with open('../corpus/shell_noun.html', 'w', encoding='utf-8') as file:
     df.to_html(file)
