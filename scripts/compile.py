c8075a4c37d943c456bd423511a9e00bb00997ef
August Chao <august.chao@twsc.io>
2024-01-08 17:50:57 +0800

    update corpus

--- scripts/compile.py
+++ scripts/compile.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv("../corpus/shell_noun.csv")
+df = pd.read_csv("./corpus/shell_noun.csv")
 df.columns = ['cate1', 'cate2', 'cate3', 'trans_ch', 'trans_jp', 'trans_cz', "trans_vie", "sent_vie", "sent_eng", "sent_jp", "sent_cz", "sent_ch"]
 df.fillna('', inplace=True)
 
-with open('../corpus/shell_noun.html', 'w', encoding='utf-8') as file:
+with open('./corpus/shell_noun.html', 'w', encoding='utf-8') as file:
     df.to_html(file)
 
 print("shell_noun.html ready!")
